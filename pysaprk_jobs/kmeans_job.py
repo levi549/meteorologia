00a909fa3c7effe79f79a717caf11d538e895a47
levi549 <cartaginezzilevi@gmail.com>
2026-05-25 19:03:48 -0300

    continuação no ml kmeans junto com o pysaprk

--- pysaprk_jobs/kmeans_job.py
+++ pysaprk_jobs/kmeans_job.py
@@ -1,23 +1,36 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
-from pyspark.ml.feature import StringIndexer, OneHotEncoder, VectorAssembler, StandardScaler
+from pyspark.ml.feature import  VectorAssembler
 import os
-from pyspark.ml.feature import StringIndexer, OneHotEncoder
-
+from src.ML import ML
 spark = SparkSession.builder \
-    .appName("KMeans Clustering") \
+    .appName("KMeans_Clustering") \
     .config("spark.jars.packages", "org.postgresql:postgresql:42.7.2") \
     .getOrCreate()
 
-URL_SUPABASE =os.getenv("SUPABASE_POSTGERESQL_URL")
+URL_SUPABASE =os.getenv("SUPABASE_POSTGRESQL_URL")
 PROPRIEDADES = {
     "user": "postgres",
     "password": os.getenv("SUPABASE_PASSWORD"),
     "driver": "org.postgresql.Driver"}
 
 
-df=spark.read.jdbc(
-    url=URL_SUPABASE,
-    table="public.dados_csv_silver",
-    properties=PROPRIEDADES
-)
+def main():
+    df=spark.read.jdbc(
+        url=URL_SUPABASE,
+        table="public.dados_csv_silver",
+        properties=PROPRIEDADES
+    )
+
+
+
+    assembler=VectorAssembler(inputCols=[
+        "mes_sin",
+        "mes_cos",
+        "temp_padronizado",
+        "humidity_padronizado",
+        "pressure_padronizado"
+    ], outputCol="features")
+    df_features=assembler.transform(df)
+    kmeans=ML()
+    kmeans.treino(df_features.select("features"))
--- src/ML.py
+++ src/ML.py
@@ -1,8 +1,13 @@
-from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
+from pyspark.ml.clustering import KMeans
 class ML:
     def __init__(self):
-        self.kmeans = KMeans(n_clusters=3, random_state=0)
+        self.kmeans = KMeans(featuresCol="features", k=3, seed=0)
 
     def treino(self, data):
-        self.kmeans.fit(data)
+        try:
+            self.modelo=self.kmeans.fit(data)
+            self.resultado=self.modelo.transform(data)
+            print("Treinamento do modelo KMeans concluído com sucesso.")
+        except Exception as e:
+            print(f"Ocorreu um erro durante o treinamento do modelo KMeans: {e}")
+            raise e
