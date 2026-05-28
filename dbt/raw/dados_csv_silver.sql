f1f2f7eb6760fa793173a825b1f6c75ad14b1bb8
levi549 <cartaginezzilevi@gmail.com>
2026-05-28 18:15:31 -0300

    refatoreação de estrutura da taebla silver

--- dbt/raw/dados_csv_silver.sql
+++ dbt/raw/dados_csv_silver.sql
@@ -1,21 +1,34 @@
 with dados_raw_csv as (
     select city_id,city_name,dt,temp,humidity,pressure,weather_main,anomaly_name from {{source('fonte_supabase','raw_csv')}}
+),
+
+ dados_padronizados as (
+    select 
+        city_id,
+        city_name,
+        dt,
+        temp,
+        humidity,
+        pressure,
+        weather_main,
+        anomaly_name,
+        SIN(EXTRACT(MONTH FROM dt::DATE) * 2 * PI() / 12) AS mes_sin,
+        COS(EXTRACT(MONTH FROM dt::DATE) * 2 * PI() / 12) AS mes_cos,
+        PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY temp) OVER(PARTITION BY city_id) AS temp_mediana,
+        PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY humidity) OVER(PARTITION BY city_id) AS humidity_mediana,
+        PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY pressure) OVER(PARTITION BY city_id) AS pressure_mediana
+    from dados_raw_csv
 )
-
-
 
 
 SELECT 
     {{ dbt_utils.generate_surrogate_key(['city_id', 'dt']) }} AS id, 
     city_name,
-    SIN(EXTRACT(MONTH FROM dt::DATE) * 2 * PI() / 12) AS mes_sin,
-    COS(EXTRACT(MONTH FROM dt::DATE) * 2 * PI() / 12) AS mes_cos,
-    (temp - AVG(temp) OVER(PARTITION BY city_id)) / 
-        NULLIF(STDDEV(temp) OVER(PARTITION BY city_id), 0) AS temp_padronizado,     
-    (humidity - AVG(humidity) OVER(PARTITION BY city_id)) / 
-        NULLIF(STDDEV(humidity) OVER(PARTITION BY city_id), 0) AS humidity_padronizado,
-    (pressure - AVG(pressure) OVER(PARTITION BY city_id)) / 
-        NULLIF(STDDEV(pressure) OVER(PARTITION BY city_id), 0) AS pressure_padronizado,
+    mes_sin,
+    mes_cos,
+    COALESCE(temp, temp_mediana) AS temp,
+    COALESCE(humidity, humidity_mediana) AS humidity,
+    COALESCE(pressure, pressure_mediana) AS pressure,
     weather_main,
     anomaly_name
-FROM dados_raw_csv
+FROM dados_padronizados
