f70b0d70cdb71056f1aecb877670ae1d483806c9
levi549 <cartaginezzilevi@gmail.com>
2026-06-04 16:03:07 -0300

    finalização da poo das ML

--- src/ML.py
+++ src/ML.py
@@ -3,15 +3,9 @@ from pyspark.ml.classification import RandomForestClassifier
 from abc import ABC, abstractmethod
 
 class ML(ABC):
-    def __init__(self,predictionCol,labelCol):
-        self.Modelo=RandomForestClassifier(
-        featuresCol="features",
-        labelCol=labelCol,
-        predictionCol=predictionCol,
-        numTrees=100,    
-        seed=0,
-        maxDepth=5,
-        maxBins=10)
+    def __init__(self):
+        self.Modelo=None
+        self.Modelo_treinado=None
     @abstractmethod
     def treino(self, data):
         pass
@@ -21,27 +15,40 @@ class ML(ABC):
 
 
 
-class ML_kmeans():
+class ML_kmeans(ML):
     def __init__(self):
-        self.kmeans = KMeans(featuresCol="features",predictionCol="prediction", k=3, seed=0)
+        super().__init__()
+        self.Modelo=KMeans(featuresCol="features",predictionCol="prediction", k=3, seed=0)
     def treino(self, data):
         try:
-            self.kmeans_treinado=self.kmeans.fit(data)
+            self.Modelo_treinado=self.Modelo.fit(data)
             print("Treinamento do modelo KMeans concluído com sucesso.")
         except Exception as e:
             print(f"Ocorreu um erro durante o treinamento do modelo KMeans: {e}")
             raise e
     def predict(self, data):
         try:
-         return self.kmeans_treinado.transform(data)
+         return self.Modelo_treinado.transform(data)
         except Exception as e:
             print(f"Ocorreu um erro durante a previsão com o modelo KMeans: {e}")
             raise e
 
 
+class ML_RandomForest(ML):
+    def __init__(self, predictionCol, labelCol):
+        super().__init__()
+        self.Modelo=RandomForestClassifier(
+        featuresCol="features",
+        labelCol=labelCol,
+        predictionCol=predictionCol,
+        numTrees=100,    
+        seed=0,
+        maxDepth=5,
+        maxBins=10)
 
 
-class ML_nivel_de_alerta(ML):
+        
+class ML_nivel_de_alerta(ML_RandomForest):
     def __init__(self):
         super().__init__(predictionCol="prediction_alerta", labelCol="Nivel_de_alerta")
 
@@ -49,7 +56,7 @@ class ML_nivel_de_alerta(ML):
     def treino(self, data):
         try:
             self.Modelo_treinado=self.Modelo.fit(data)
-            self.resultado=self.Modelo_treinado.transform(data)
+           
             print("Treinamento do modelo de classificação supervisionada de nivel de alerta concluído com sucesso.")
         except Exception as e:
             print(f"Ocorreu um erro durante o treinamento do modelo de classificação supervisionada de nivel de alerta: {e}")
@@ -62,15 +69,13 @@ class ML_nivel_de_alerta(ML):
             print(f"Ocorreu um erro durante a previsão com o modelo de classificação supervisionada de nivel de alerta: {e}")
             raise e
 
-
-class Ml_anomaly(ML):
+class Ml_anomaly(ML_RandomForest):
     def __init__(self):
         super().__init__(predictionCol="prediction_anomaly", labelCol="anomaly_name")
 
     def treino(self, data):
         try:
             self.Modelo_treinado=self.Modelo.fit(data)
-            self.resultado=self.Modelo_treinado.transform(data)
             print("Treinamento do modelo de classificação de anomalias concluído com sucesso.")
         except Exception as e:
             print(f"Ocorreu um erro durante o treinamento do modelo de classificação de anomalias: {e}")
