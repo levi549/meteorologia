bfcf2ce952b458a7ec8a958af6e9bdf49c8c3178
levi549 <cartaginezzilevi@gmail.com>
2026-06-09 20:23:00 -0300

    continuação da refatoração da arquitetura

--- dbt/silver_gold/gold_dados_kmeans.sql
+++ dbt/silver_gold/gold_dados_kmeans.sql
@@ -1,7 +1,9 @@
 {{ config(
     materialized='incremental',
     unique_key='id',
-)}}
+    on_schema_change='fail',
+    incremental_strategy='merge'
+) }}
 
 with dados_csv_silver as(
     select
@@ -14,28 +16,44 @@ with dados_csv_silver as(
         pressure,
         anomaly_name
         from {{ref('dados_csv_silver')}}
+    {% if is_incremental() %}
+      where id not in (select id from {{ this }})
+    {% endif %}
 ),
 
+estatisticas_cidade as (
+    select
+        city_name,
+        avg(temp) as temp_media,
+        NULLIF(stddev(temp), 0) as temp_desvio_padrao,
+        avg(humidity) as humidity_media,
+        NULLIF(stddev(humidity), 0) as humidity_desvio_padrao,
+        avg(pressure) as pressure_media,
+        NULLIF(stddev(pressure), 0) as pressure_desvio_padrao
+    from {{ref('dados_csv_silver')}}
+    group by city_name
+),
 
 dados_padronizados as (
     select 
-    id,
-    mes_sin,
-    mes_cos,
+    d.id,
+    d.mes_sin,
+    d.mes_cos,
     case 
-        when stddev(temp) OVER( PARTITION BY city_name)=0 then 0
-        else (temp - avg(temp) OVER( PARTITION BY city_name)) / NULLIF(stddev(temp) OVER(PARTITION BY city_name), 0)
+        when e.temp_desvio_padrao=0 then 0
+        else (d.temp - e.temp_media) / NULLIF(e.temp_desvio_padrao, 0)
     end as temp_padronizado,
     case 
-        when stddev(humidity) OVER( PARTITION BY city_name)=0 then 0
-        else (humidity - avg(humidity) OVER( PARTITION BY city_name)) / NULLIF(stddev(humidity) OVER(PARTITION BY city_name), 0)
+        when e.humidity_desvio_padrao=0 then 0
+        else (d.humidity - e.humidity_media) / NULLIF(e.humidity_desvio_padrao, 0)
     end as humidity_padronizado,
     case 
-        when stddev(pressure) OVER( PARTITION BY city_name)=0 then 0
-        else (pressure - avg(pressure) OVER( PARTITION BY city_name)) / NULLIF(stddev(pressure) OVER(PARTITION BY city_name), 0)
+        when e.pressure_desvio_padrao=0 then 0
+        else (d.pressure - e.pressure_media) / NULLIF(e.pressure_desvio_padrao, 0)
     end as pressure_padronizada,
-    anomaly_name
-    from dados_csv_silver
+    d.anomaly_name
+    from dados_csv_silver d
+    LEFT JOIN estatisticas_cidade e using (city_name)
     )
 
 
